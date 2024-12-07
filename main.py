e4756bd9ce7ea48cbf8e8f2db8586570144a37ec
miilodii <akparalievaaltynai@icloud.com>
2024-12-07 19:16:24 +0600

    адилет: добавил файл module_altynai.py; изменил файл main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+from module_altynai import STUDENTS_GRADES, STUDENTS_LIST
+
+def show_students(students):
+    for i in students:
+        print(i)
+
+show_students(STUDENTS_LIST)
